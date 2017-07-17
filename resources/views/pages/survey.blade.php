f331e2598326072ce6af6652e24a32c48e733539
miguelbc7 <miguel.lm21@gmail.com>
2017-07-17 12:47:42 -0430

    add surveys options

--- resources/views/pages/survey.blade.php
+++ resources/views/pages/survey.blade.php
@@ -1,10 +1,28 @@
+
 @foreach($surquestions as $sq)
 	{{ $sq->question }}
-	<ul>
-	<li>Muy Malo</li>
-	<li>Malo</li>
-	<li>Regular</li>
-	<li>Bueno</li>
-	<li>Muy Bueno</li>
-	</ul>
+	
+	<div class="radio{{ $sq->position }}">
+	  <label>
+	    <input type="radio" name="optionsRadios" id="optionsRadios{{ $sq->position }}1" value="1">
+	    Muy Malo
+	  </label>
+	  <label>
+	    <input type="radio" name="optionsRadios" id="optionsRadios{{ $sq->position }}2" value="2">
+	    Malo
+	  </label>
+	  <label>
+	    <input type="radio" name="optionsRadios" id="optionsRadios{{ $sq->position }}3" value="3">
+	    Regular
+	  </label>
+	  <label>
+	    <input type="radio" name="optionsRadios" id="optionsRadios{{ $sq->position }}4" value="4">
+	    Bueno
+	  </label>
+	  <label>
+	    <input type="radio" name="optionsRadios" id="optionsRadios{{ $sq->position }}5" value="5">
+	    Muy Bueno
+	  </label>
+	</div>
+	<br>
 @endforeach
