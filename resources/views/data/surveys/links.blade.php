c4c4bef4c70b6123591a25b5f53f5ad5e1a99203
miguelbc7 <miguel.lm21@gmail.com>
2017-07-27 16:09:21 -0430

    Add tooltips in links

--- resources/views/data/surveys/links.blade.php
+++ resources/views/data/surveys/links.blade.php
@@ -11,14 +11,18 @@
 @section('main-content')
 
 <div>Hipervinculo Directo</div>
-<a href="{{ url('surveys/'.$survey->id.'/survey') }}">127.0.0.1:8000/surveys/{{ $survey->id }}/survey</a>
+	<div class="col-xs-1 col-sm-1 col-md-3 col-lg-4 col-xl-4" data-toggle="tooltip" title data-original-title="Hipervinculo a la encuesta en formato normal">
+		<a href="{{ url('surveys/'.$survey->id.'/survey') }}">127.0.0.1:8000/surveys/{{ $survey->id }}/survey</a>
+	</div>
 
 <br>
 
 <div>Codigo QR</div>
-<a href="{{ url('surveys/'.$survey->id.'/survey') }}"><img src="data:image/png;base64,{{DNS2D::getBarcodePNG('http://127.0.0.1:8000/surveys/'.$survey->id.'/survey', 'QRCODE')}}" alt="barcode" /></a>
+	<div class="col-xs-1 col-sm-1 col-md-2 col-lg-2 col-xl-2" data-toggle="tooltip" title data-original-title="Hipervinculo a la encuesta en formato de codigo QR">
+		<a href="{{ url('surveys/'.$survey->id.'/survey') }}"><img src="data:image/png;base64,{{DNS2D::getBarcodePNG('http://127.0.0.1:8000/surveys/'.$survey->id.'/survey', 'QRCODE')}}" alt="barcode" /></a>
+	</div>
 
-<div class="box-body">
+<div class="box-body col-xs-1 col-sm-1 col-md-6 col-lg-12 col-xl-12">
 	<div class="pull-right">
 		{!!link_to_route('surveys.index', $title = 'Atras',  $parameters = '', $attributes = ['class' => 'btn btn-default btn3d', 'style'=>'-webkit-border-radius: 8px;-moz-border-radius: 8px;border-radius: 8px;'])!!}
 	</div>
